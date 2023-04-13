b73f7b550917586378886583164227a8882e8c37
Hanbooyo <qnfudk@gmail.com>
2023-04-13 16:52:52 +0900

    DB자료 읽어오기

--- app.py
+++ app.py
@@ -17,15 +17,6 @@ db_conn = pymysql.connect(
 
 print(db_conn)
 
-# 커서 객체 생성
-cursor = db_conn.cursor()
-
-query = "select * from player"
-
-cursor.execute(query) #query 실행
-
-for i in cursor: #커서가 받아온 정보를 출력
-    print(i)
 
 #Flask객체 인스턴스 생성
 app = Flask(__name__) # app이라는 플라스크 인스턴스 생성
@@ -49,6 +40,43 @@ def testpost():
     value = request.form['input'] #'input 이라는 이름의 값을 받음
     print(value)
     return render_template('posttest.html')
+
+@app.route('/sqltest')
+def sqltest():
+        # 커서 객체 생성
+    cursor = db_conn.cursor()
+
+    query = "select * from player"
+
+    cursor.execute(query) #query 실행
+
+    result = []
+
+    for i in cursor: #커서가 받아온 정보를 출력
+        temp = {'player_id':i[0], 'player_name':i[1]} #받아온 자료들의 인덱스를 이용해서 담음
+        result.append(temp)
+
+    return render_template('sqltest.html', result_table = result)
+
+@app.route('/detail')
+def detailtest():
+    temp = request.args.get('id')
+    temp1 = request.args.get('name')
+        # 커서 객체 생성
+    cursor = db_conn.cursor()
+
+    query = "select * from player where player_id = {} and player_name like '{}'".format(temp,temp1)
+
+    cursor.execute(query) #query 실행
+
+    result = []
+
+    for i in cursor: #커서가 받아온 정보를 출력
+        temp = {'player_id':i[0], 'player_name':i[1], 'team_name':i[2], 'height':i[-2], 'weight':i[-1]} #받아온 자료들의 인덱스를 이용해서 담음
+        result.append(temp)
+
+    return render_template('detail.html', result_table = result)
+
 
 
 @app.route('/login')
